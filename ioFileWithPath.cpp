a8287c35a7012869093c1a61a26767e8a8a9fd6e
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-06-09 18:19:02 +0700

    pemanggil variabel dari baris dan namafile

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+}
