d608e5ed261d1fc331146ebbc2a6335f231bdb78
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-06-09 17:22:45 +0700

    membuka file

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,5 +8,7 @@ int main()
     string baris;
 
     // membuat obyek output file stream dalam mode menulis
-    ofstream.outfile;
+    ofstream outfile;
+    // membuka file
+    outfile.open("contohfile.txt");
 }
