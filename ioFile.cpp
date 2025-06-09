74389b7cfc955832e0305678149db60d674d201b
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-06-09 17:21:55 +0700

    membuat obyek output file

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+
+    // membuat obyek output file stream dalam mode menulis
+    ofstream.outfile;
+}
