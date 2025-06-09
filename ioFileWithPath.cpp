9fe08360f3801a56b5d934a29cd7a044d36f2838
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-06-09 18:42:18 +0700

    tutup dan tampilkan program

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,4 +30,30 @@ int main()
         // menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+     // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuat obyek input file stream dalam mode membaca
+    ifstream infile;
+    //membuka file
+    infile.open(NamaFile + ".txt");
+
+    cout << endl
+        << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setia baris
+        while(getline(infile, baris))
+        {
+            //dan menampilkan disini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else
+        cout << "Unable to open file";
+        return 0;
 }
